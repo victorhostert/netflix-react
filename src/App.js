0b971b555c224dc7642a35a83cf07d7f4cda445d
Victor Hostert <victorwhostert@gmail.com>
2022-03-09 08:28:17 -0300

    feat: added footer

--- src/App.js
+++ src/App.js
@@ -57,6 +57,20 @@ export default () => {
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
+
+      <footer>
+        Feito com ReactJS por Victor William Hostert <br/>
+        Direitos de imagem para Netflix <br/>
+        Dados pegos do site TheMovieDB <br/>
+        Baseado no vídeo <span><a href="https://www.youtube.com/watch?v=tBweoUiMsDg">🔥 Clone do NETFLIX em REACTJS para Iniciantes</a></span>
+      </footer>
+        
+      {movieList.length <= 0 && 
+        <div className="loading">
+          <img src="https://www.rchandru.com/images/portfolio/loading.gif" alt="Loading" />
+        </div>
+      }
+      
     </div>
   )
 }
